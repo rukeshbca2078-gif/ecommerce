3bb2e8bc2bdde91ee3ab93b77bce577569cc553d
Rukesh Shrestha <rukeshbca2078@users.noreply.github.com>
2026-04-19 01:54:28 +0545

    update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from carts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.cart,name='cart'),
 ]
