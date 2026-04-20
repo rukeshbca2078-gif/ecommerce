b7e3b1b3f5fafd2b38eb9fa1d38c918b5b2d2e25
Rukesh Shrestha <rukeshbca2078@users.noreply.github.com>
2026-04-20 08:55:58 +0545

    update

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
 # Create your models here.
+
+class payment(models.Model):
+    order_id = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+
 # Create your models here.
